13a3a8afe0b377567798367e412f61be02c5ae1d
shinws <shinws@vt.edu>
2020-12-05 11:23:48 -0800

    port stat and link delay

---
+++ src/link_delay_check.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys, re
+
+from time import sleep
+
+def main(argv):
+	while True:
+		if len(sys.argv) == 1:
+			print("usage : delay_check.py <src host number> <dst host number>")
+			sys.exit()
+		str = '10.0.0.'
+		src_host = argv[1]
+		dst_host = argv[2]
+		dst_host_ip = str + dst_host
+		#print(dst_host_ip)
+
+		import subprocess
+		process = subprocess.Popen(['ping', '-c3', dst_host_ip ],
+				     stdout=subprocess.PIPE, 
+				     stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		#print(stdout, stderr)
+		stdout = stdout.decode("utf-8")
+		# ping avg index is 4
+		f = open('link_delay.log', 'a')
+		f.write(src_host +" "+ dst_host+ " "+ stdout.split("/")[4] + "\n")
+				
+		'''for line in stdout:
+			print('each line', line)
+			if re.search('rtt', line):
+				print (line)'''
+
+if __name__ == "__main__":
+	main(sys.argv)
--- src/portstat_monitor.py
+++ src/portstat_monitor.py
@@ -6,17 +6,69 @@ from time import sleep
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from pprint import pprint
+
+import re
 
 
 def portstat_check(hosts, devices, links, graph):
+	before_bw = 0
 	#format [(dev id, flow id, port no, type, src mac, dst mac, out port, out type), ...]
 	print(links)
-	while True:
-		# get max bw per links
-		# get current bw per links
-		# 
-		# get delay
-		# is the delay value custom? or 
+	stats = get_stats()
+	pprint(stats)
+	# get max bw per links
+	ind = -1
+	for link in links:
+		ind = ind + 1
+		if (link[0] == 'switch-switch'):
+			src_sw = 's' + re.findall('\d+', link[1])[0].lstrip('0') + '-' + str(link[2])
+			dest_sw = 's' + re.findall('\d+', link[3])[0].lstrip('0') + '-' + str(link[4])
+			max_bw = link[4] * 1000 * 1000
+			#print(typeof(stats))
+			curr_bw = (stats[link[1]][link[2]]['bytesRx'] + stats[link[1]][link[2]]['bytesTx']) / 8
+			print(src_sw, dest_sw, max_bw, curr_bw)
+			links[ind][7] = curr_bw / 1000000 - links[ind][7]
 
-		sleep(1)
+	# get current bw per links
+	# 
+	# get delay
+	# is the delay value custom? or 
 
+'''for host in hosts_dict:
+                host_mac = host['mac']
+                dst_dev = host['locations'][0]['elementId']
+                dst_port = int(host['locations'][0]['port'] )
+
+                bandwidth, delay = find_config_match(links_config, ['h', 's'], [host_mac, dst_dev])
+
+                links.append( ('host-switch', host_mac, -1, dst_dev, dst_port, bandwidth, delay) )
+
+                hosts.append(host_mac)
+
+
+        for dev in devs_dict:
+                dev_id = dev['id']
+                devices.append(dev_id)
+
+        for link in links_dict:
+                dst_dev = link['dst']['device']
+                dst_port = int(link['dst']['port'])
+                src_dev = link['src']['device']
+                src_port = int(link['src']['port'])
+
+                bandwidth, delay = find_config_match(links_config, ['s', 's'], [src_dev, dst_dev])
+
+                links.append( ('switch-switch', src_dev, src_port, dst_dev, dst_port, bandwidth, delay) )
+
+        devices = list(set(devices))
+        links= list(set(links))
+
+        for host in hosts:
+                graph.add_node(host, type='host')
+        for device in devices:
+                graph.add_node(device, type='device')
+        for link in links:
+                graph.add_edge(link[1], link[3], weight = 1.0) #can add weight based on delay
+
+'''
