79d1f328113f8494d98efcd577c575c34459bc31
Sumved <sumved.ravi@gmail.com>
2020-12-05 08:46:55 -0800

    added add_flow and delete_flow functionality

--- src/onos_interface.py
+++ src/onos_interface.py
@@ -13,10 +13,37 @@ from pprint import pprint
 from requests.auth import HTTPBasicAuth
 
 
-def add_flow(_print = False):
+# src_mac, dst_mac are strings 
+# in_port, out_port are ints
+def add_flow(src_mac, dst_mac, in_port, out_port, priority = 100, _print = False):
+	data = { \
+		'isPermanent': True, \
+		'priority': priority, \
+		'selector': {'criteria': [{'port': in_port, 'type': 'IN_PORT'},{'mac': dst_mac,'type': 'ETH_DST'}, {'mac': src_mac, 'type': 'ETH_SRC'}]}, \
+		'treatment': {'deferred': [], 'instructions': [{'port': out_port, 'type': 'OUTPUT'}]} \
+	}
+
+	json_data = json.dumps(data)	
+	reply = rq.post(get_url, auth = HTTPBasicAuth(ONOS_USER, ONOS_PASS), data = json_data)
+
+	if _print:
+		print(reply.text)
+
 	return None
 
-def delete_flow(_print = False):
+# device id must be in form 'of:00000000000000xx', where xx is the device number
+# flow id, is of form: 92042317780586387 .. this id can be found from get_flows() output dict 
+def delete_flow(device_id, flow_id, _print = False):
+
+	DEVICEID = 'of:0000000000000001'
+	FLOWID = '92042317780586387'
+
+	get_url = 'http://%s:%d/onos/v1/flows/%s/%s' % (ONOS_IP, ONOS_PORT, device_id, flow_id)
+	reply = rq.delete(get_url, auth = HTTPBasicAuth(ONOS_USER, ONOS_PASS))
+
+	if _print:
+		print(reply.text)
+
 	return None
 
 def get_hosts(_print = False):
