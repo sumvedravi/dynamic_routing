88cf62da0948c61c17c17723c9fc54ba4c9bdfab
shinws <shinws@vt.edu>
2020-12-07 10:03:00 -0800

    calculate ef value

--- src/portstat_monitor.py
+++ src/portstat_monitor.py
@@ -8,9 +8,11 @@ import subprocess
 import networkx as nx
 import matplotlib.pyplot as plt
 from pprint import pprint
+from sklearn import preprocessing
 
 
-def check_portstats(links):
+
+def check_portstats(links, graph):
 	stats = get_stats()
 	# get max bw per links
 	
@@ -26,6 +28,8 @@ def check_portstats(links):
 					bw_per_sec = curr_bw - links[src_dev][dst_dev]['bw_before']
 					links[src_dev][dst_dev]['bw_before'] = curr_bw
 					links[src_dev][dst_dev]['bw'] = bw_per_sec
+					# onos portstat show only network devices' ports stats
+					# use h->s stats with the same value s->h
 					if dst_dev[0:2] != 'of':
 						links[dst_dev][src_dev]['bw_before'] = curr_bw
 						links[dst_dev][src_dev]['bw'] = bw_per_sec
@@ -33,7 +37,6 @@ def check_portstats(links):
 					# get delay	
 					# helper script : link_check_delay.py h1 s1
 					for line in reversed(list(open("../log/link_delay.log"))):
-						#print(line.rstrip(), " each line")
 						line = line.rstrip()
 						line_src_dev = line.split(" ")[0]
 						line_dst_dev = line.split(" ")[1]
@@ -51,3 +54,37 @@ def check_portstats(links):
 							links[dst_dev][src_dev]['delay'] = line_delay
 							links[src_dev][dst_dev]['delay'] = line_delay
 
+	# caculate normalized value 
+	bw_norm = []
+	delay_norm = []
+	for src in links:
+		for dst in links[src]:
+			if links[src][dst]['bw'] != -1:
+				bw_norm.append( links[src][dst]['bw'] )
+			if links[src][dst]['delay'] != -1:
+				delay_norm.append( links[src][dst]['delay'] )
+	bw_norm = preprocessing.normalize([bw_norm])[0]
+	delay_norm = preprocessing.normalize([delay_norm])[0]
+	bw_norm_ind = 0
+	delay_norm_ind = 0
+	for src in links:
+		for dst in links[src]:
+			if links[src][dst]['bw'] != -1:
+				links[src][dst]['bw_norm'] = bw_norm[bw_norm_ind]
+				bw_norm_ind += 1
+			if links[src][dst]['delay'] != -1:
+				links[src][dst]['delay_norm'] = delay_norm[delay_norm_ind]
+				delay_norm_ind += 1
+				
+
+	# calculate efficiency per link
+	alpha = 1
+	beta = 1
+	for src in links:
+		for dst in links[src]:
+			if links[src][dst]['delay_norm'] != -1 and links[src][dst]['bw_norm'] != -1:
+				links[src][dst]['efficency'] = alpha * links[src][dst]['bw_norm'] / \
+					links[src][dst]['link_flow_count'] + \
+					beta * links[src][dst]['delay_norm']
+				print(links[src][dst]['efficency'])
+				graph[src][dst]['weight'] = links[src][dst]['efficency']	
